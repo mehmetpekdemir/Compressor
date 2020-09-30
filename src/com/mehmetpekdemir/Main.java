18587be5d1c051ff254dba83d1b8f4b668bf3a54
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-09-30 21:09:42 +0300

    Main class created.

---
+++ src/com/mehmetpekdemir/Main.java
@@ -0,0 +1,34 @@
+package com.mehmetpekdemir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import com.mehmetpekdemir.compressor.Compressor;
+import com.mehmetpekdemir.compressor.SimpleCompressor;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Compressor compressor = new SimpleCompressor();
+
+		byte[] original = Files.readAllBytes(Paths.get("temp.txt"));
+
+		byte[] compressed = compressor.compress(original);
+
+		System.out.println("Original Size : " + original.length);
+		System.out.println("Compressed Size : " + compressed.length);
+
+		byte[] decompressed = compressor.decompress(compressed);
+
+		System.out.println("is it equal : " + Arrays.equals(original, decompressed));
+
+	}
+
+}
